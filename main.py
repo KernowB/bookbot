c10401cad467c4dbe78d077a90ca2d88cd371d63
KernowB <mlloyd@byteitcornwall.co.uk>
2025-01-29 16:54:25 +0000

    word count

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        
+    print(f"{word_count(file_contents)} words")
+
+def word_count(file_contents):
+    words = file_contents.split()
+    return len(words)
+    
+
+if __name__ == "__main__":
+    main()
